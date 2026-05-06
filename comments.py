d5c19c72289446401db44902ca204a3efa57a363
tchingonzo <tk@iamtakunda.com>
2026-05-06 13:20:29 +0100

    Updating the full project structure

---
+++ comments.py
@@ -0,0 +1,14 @@
+x = 5 # inline comment
+
+# This is a single line comment to explain the code below 
+y = x + 10 
+z = y*2 + x
+
+'''
+This is a 
+multi-line comment that can span multiple lines.
+It is often used to provide detailed explanations or documentation for code.
+In Python, multi-line comments are typically created using triple quotes ). 
+'''
+
+statetax = 0.07 # 7% state tax rate
